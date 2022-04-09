45b56006a481b0128911b0bde8e8c9f4c2c6f531
neoshua <1662282250@qq.com>
2022-04-09 09:27:53 +0800

    Update start.js
    增加了注释，优化了坐标点

--- start.js
+++ start.js
@@ -1,11 +1,23 @@
 var fl = true
+
+//立即支付坐标点
+var x = device.width  * (833 / 1080) 
+var y = device.height * (2284 / 2340) 
+
+
+
+
 threads.start(function() {
   //选择时间
   while ( true ) {
    	id("vp_dialog_select_time_content").findOne().children().forEach(child => {
       var j = 1;
 			var target = child.findOne(id("rv_selected_hour")).children().forEach(timechild => {
-         timechild.click() 
+        //默认选择第一个时间段，需要修改其他时间段需要手动  修改，如：选择第二个时间段：j == 2
+        if(j == 1){
+          timechild.click()
+        }
+          j++
       });
 		});
     
@@ -21,18 +33,18 @@ threads.start(function() {
   }
 });
 threads.start(function() {
-  //点击购物车
+  //当出现送达时间失效时，直接坐标点击 立即支付
   while (true) {
     sleep(2)
     id("bottom_style_two").waitFor()
     log("直接点击支付")
-    press(883,2284, 2)
+   press(x,y, 2)
     
   }
 });
 
 threads.start(function() {
-  //点击购物车
+  //点击购物车，如果购物车没有刷新，需要点击我的，重新刷新
   while (true) {
     log('进入购物车')
    	id("rl_car_layout").waitFor();
@@ -43,7 +55,7 @@ threads.start(function() {
 threads.start(function() {
   //结算
    while (true) {
-       log('准备结算')
+      log('准备结算')
      id("vg_car").waitFor();
      id("vg_car").findOne().children().forEach(child => {
 			var target = child.findOne(id("btn_submit"));
@@ -53,21 +65,23 @@ threads.start(function() {
    }
 });
 threads.start(function() {
-  //支付
+  //支付，基于坐标的
   while (fl) {
     log('111准备支付')
     id("tv_submit").waitFor()
 //    	id("tv_submit").findOne().click()
-    press(883,2284, 2)
+    
+    press(x,y, 2)
   }
 });
 threads.start(function() {
-  //支付
+  //支付，由于断货，需要在这里点击支付
   while (true) {
     log('222准备支付')
     id("ll_reload_action").findOne().click()
   }
 });
+
 
 
 
